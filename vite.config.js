5674c2e6c82920f547d21ad129bbd2c58895828e
Drswith <drswith@outlook.com>
2025-05-08 16:37:49 +0800

    feat: 更新 package.json，添加库描述、作者信息及关键字，升级版本至 0.1.0；引入 vite-plugin-dts 插件以支持类型定义；移除不必要的 vite.svg 文件。

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path from 'node:path'
-// import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   resolve: {
@@ -9,12 +9,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    // visualizer({
-    //   open: true, // 注意这里要设置为true，否则无效
-    //   filename: 'stats.html', // 分析图生成的文件名
-    //   gzipSize: true, // 收集 gzip 大小并将其显示
-    //   brotliSize: true, // 收集 brotli 大小并将其显示
-    // }),
+    dts(),
   ],
   server: {
     port: 5173,
@@ -37,7 +32,6 @@ export default defineConfig({
     environment: 'jsdom',
   },
   build: {
-    // minify: 'terser',
     lib: {
       entry: './src/index.ts',
       name: 'VanillaRouter',
